47c7bb29b7b79a8064f3932d32c1217aad2df347
Brandt-Ignacio <81595011+Brandt-Ignacio@users.noreply.github.com>
2025-10-06 10:48:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ async function startApp() {
     console.log("Iniciando conexión a la base de datos...");
     await MongoDBClient.connect();
     
-    console.log("Conectando APIs...")
-    
+    console.log("Conexión a la base de datos establecida. Ejecutando test de la API...");
+    // Llama a la función que inicia el procesamiento
+    await procesarEntrada('audio', 'PTT-20250801-WA0022.opus');
+    console.log("Test de la API finalizado.");
 }
 
 startApp();
